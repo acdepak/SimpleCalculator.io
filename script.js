ee5edf861e4a26650689dc30a4a37edd2c9cbb62
acdepak <acdepak@gmail.com>
2023-01-02 22:03:03 +0545

    Clear function added

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ function clearscreen() {
     document.getElementById("display_a").value="";
 }
 
+function backspace(){
+    let val = document.getElementById("display_q").value;
+    let valLength = val.length;
+    let final_q = val.slice(0 ,valLength - 1);
+    document.getElementById("display_q").value = final_q;
+}
+
 function display(value) {
     document.getElementById('display_q').value += value;
     console.log(document.getElementById('display_q').value);
